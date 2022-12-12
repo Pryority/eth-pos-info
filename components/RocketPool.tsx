d68c623e17ef2e9c800676d72f590b1faa71f4e4
pryority <matthewapryor@gmail.com>
2022-12-12 02:40:57 -0500

    minor styling

--- components/RocketPool.tsx
+++ components/RocketPool.tsx
@@ -7,11 +7,14 @@ type Props = {}
 export const RocketPool = (props: Props) => {
   return (
     <div className="flex flex-col space-y-2 text-sm sm:text-base md:text-lg bg-clip-text text-transparent bg-gradient-to-b from-white via-white/90 to-white/50">
-      <div className="flex items-center space-x-4">
-        <div className="flex items-center space-x-1 bg-clip-text text-transparent bg-gradient-to-tr from-orange-500 via-orange-400 to-yellow-600">
-          <Link href={"https://rocketpool.net"} className="font-bold ">Rocket Pool</Link><ArrowTopRightOnSquareIcon className="h-4 w-4 text-orange-500"/>
+      <div className="flex flex-col text-center items-center sm:flex-row sm:space-x-4 md:space-x-8 tracking-wider pb-2">
+        <div className="flex items-center bg-clip-text text-transparent bg-gradient-to-tr from-orange-500 via-orange-400 to-yellow-600">
+          <Link href={"https://rocketpool.net"} className="font-bold tracking-tighter uppercase md:text-lg w-[90px] sm:w-[110px] md:w-[120px]">
+            Rocket Pool
+          </Link>
+          <ArrowTopRightOnSquareIcon className="h-4 w-4 text-orange-500"/>
         </div> 
-        <div className="text-orange-500/70">
+        <div className="text-orange-500/70 text-xs sm:text-start sm:text-sm md:text-lg">
           A base layer protocol for <span className="underline">decentralized and trustless ETH staking</span>.
         </div>
       </div>
@@ -22,6 +25,14 @@ export const RocketPool = (props: Props) => {
 
       <span>
         The core premise behind a protocol is to ensure the network is not beholden to any one party. This is a principle directly linked to Ethereum and ETH2 itself, and a mindset used at every stage of the process as we develop Rocket Pool for its upcoming launch.
+      </span>
+
+      <span>
+        Rocket Pool strives to embody the core ethos of Ethereum and DeFi, specifically the non-custodial, trustless nature that allows self-sovereignty to truly thrive.
+      </span>
+      
+      <span>
+        This is why creating the protocol layer for ETH2 staking is so important, especially with the vast majority of players either not having the technical skills to run a node, or the financial capacity to own 32 ETH.
       </span>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import {ArrowTopRightOnSquareIcon} from "@heroicons/react/24/outline";
+import {ArrowDownIcon, ArrowTopRightOnSquareIcon} from "@heroicons/react/24/outline";
 import Nav from "../components/Nav";
 import rocketPoolLogo from "../rocketpool.png";
 import {RocketPool} from "../components/RocketPool";
@@ -26,12 +26,18 @@ export default function Home() {
           
           <span><Link href={"https://ethereum.org/en/upgrades/merge/"} className="transition-all ease-in-out hover:text-orange-400 duration-75">ETH2 </Link> currently has a <Link href={"https://ethereum.org/en/developers/docs/consensus-mechanisms/pos/"} className="transition-all ease-in-out hover:text-yellow-300 duration-75">Proof of Stake (PoS)</Link> chain called the <Link href={"https://beaconcha.in/"} className="transition-all ease-in-out hover:text-teal-300 duration-75">Beacon Chain</Link>, <Link href={"https://beaconcha.in/"} className="transition-all ease-in-out hover:text-green-500 duration-75">which is faster, more energy efficient and more decentralised</Link> than <Link href={"https://ethereum.org/en/developers/docs/consensus-mechanisms/pow"} className="transition-all ease-in-out hover:text-stone-400 duration-75">PoW</Link>. {"It's"} <Link href={"https://rocketpool.net/"} className="transition-all ease-in-out hover:text-cyan-400 duration-75">powered by users depositing ETH and providing an Ethereum node online 24/7 to perform the required validation</Link>. As a reward for providing the node, the <Link href={"https://beaconcha.in/"} className="transition-all ease-in-out hover:text-teal-300 duration-75">Beacon Chain</Link> gives node operators additional ETH on top of their deposits. It can be viewed similarly to earning interest in a bank account, with the exception that these rewards are generated in return for helping secure the network.</span>
 
+          <iframe className="rounded border-2" src="https://dune.com/embeds/991513/1716891/0f7acad0-41b3-4c0f-ba03-ef43aa0b16f2" height="500" width="100%" title="chart 3"/>
+
           <Link href="https://vitalik.eth.limo/general/2017/12/31/pos_faq.html#what-is-proof-of-stake" className="flex items-center space-x-1 transition-all ease-in-out hover:text-pink-300 duration-75 w-full justify-center md:justify-end">
             <span className="text-[11px] sm:text-sm"><span className="font-medium">{"Vitalik's"}</span> explanation of Proof-Of-Stake (POS)</span>
             <ArrowTopRightOnSquareIcon className="h-4 w-4 font-bold"/>
           </Link>
 
           {/* <Nav /> */}
+          <div className="flex flex-col items-center text-center space-y-4 py-40 text-white/50">
+            <div className="">A powerful solution from maximal decentralization</div>
+            <ArrowDownIcon className="h-4 w-4"/>
+          </div>
 
           <div className="py-8 flex flex-col items-center space-y-8">
             <Link href={"https://rocketpool.net"}>
@@ -41,9 +47,8 @@ export default function Home() {
           </div>
 
           <iframe className="rounded border-2" src="https://dune.com/embeds/609518/1138523/5a23e409-945f-400d-b979-5d8fffb8f732" height="500" width="100%" title="chart 1"/>
-          <div className="flex space-x-2 items-center">
+          <div className="flex flex-col space-y-8 md:space-x-2 items-center">
             <iframe className="rounded border-2" src="https://dune.com/embeds/991483/1716887/f86ab809-0549-44d0-ae01-f2865e83279d" height="500" width="100%" title="chart 2"/>
-            <iframe className="rounded border-2" src="https://dune.com/embeds/991513/1716891/0f7acad0-41b3-4c0f-ba03-ef43aa0b16f2" height="500" width="100%" title="chart 3"/>
           </div>
         </div>
       </main>

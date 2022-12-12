e520232ebea62b3fc8a2d69bf2f108c1bb4dc4c2
pryority <matthewapryor@gmail.com>
2022-12-12 10:52:50 -0500

    add styling for light versus dark

--- components/RocketPool.tsx
+++ components/RocketPool.tsx
@@ -6,8 +6,8 @@ type Props = {}
 
 export const RocketPool = (props: Props) => {
   return (
-    <div className="flex flex-col space-y-2 text-sm sm:text-base md:text-lg bg-clip-text text-transparent bg-gradient-to-b from-white via-white/90 to-white/70">
-      <div className="flex flex-col space-y-2 sm:space-y-0 text-center items-center sm:flex-row sm:space-x-4 md:space-x-8 tracking-wider pb-2">
+    <div className="flex flex-col space-y-2 text-sm sm:text-base md:text-lg text-stone-700 dark:text-stone-50 bg-clip-text text-transparent bg-gradient-to-b from-white via-white/90 to-white/70">
+      <div className="flex flex-col space-y-2  sm:space-y-0 text-center items-center sm:flex-row sm:space-x-4 md:space-x-8 tracking-wider pb-2">
         <div className="flex items-center bg-clip-text text-transparent bg-gradient-to-tr from-orange-500 via-orange-400 to-yellow-600">
           <Link href={"https://rocketpool.net"} className="font-bold tracking-tighter uppercase md:text-lg w-[90px] sm:w-[110px] md:w-[120px]">
             Rocket Pool
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="justify-center font-mono flex flex-col w-full items-center bg-gradient-to-bl from-slate-50 via-slate-50 to-stone-50 dark:from-slate-900 dark:via-slate-900 dark:to-stone-800">
+    <div className="justify-center font-mono flex flex-col w-full items-center bg-gradient-to-bl from-teal-50 via-teal-100 to-orange-600/50 dark:from-slate-900 dark:via-slate-900 dark:to-stone-800">
       <Component {...pageProps} />
       <Footer/>
     </div>
